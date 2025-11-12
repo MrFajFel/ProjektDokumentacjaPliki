44486f414c3a91c5946932544b4409378cb7331f
filip <filipaleksandrowicz5@gmail.com>
2025-11-12 16:04:28 +0100

    Wiecej informacji do dokumentacji

--- projekt-dokumentacja/app/components/Sidebar.tsx
+++ projekt-dokumentacja/app/components/Sidebar.tsx
@@ -24,10 +24,23 @@ export default function Sidebar(){
                      <a href="/#komendy_podstawowe">Komendy</a>
                 </li>
                 <li className="mt-1 ml-4 w-fit p-1 pt-0 pb-0 border-l text-gray-800 hover:text-gray-400   border-l-gray-400 hover:border-black dark:text-gray-400 dark:hover:text-gray-700  dark:border-l-gray-400 dark:hover:border-l-gray-500  cursor-pointer">
+                    <a href="/#sudo-operacja">Operacja sudo</a>
+                </li>
+                <li className="mt-1 ml-4 w-fit p-1 pt-0 pb-0 border-l text-gray-800 hover:text-gray-400   border-l-gray-400 hover:border-black dark:text-gray-400 dark:hover:text-gray-700  dark:border-l-gray-400 dark:hover:border-l-gray-500  cursor-pointer">
                     <a href="/#systemowe-podstawowe-komendy">Podstawowe Komendy</a>
                 </li>
                  <li className="mt-1 ml-4 w-fit p-1 pt-0 pb-0 border-l text-gray-800 hover:text-gray-400   border-l-gray-400 hover:border-black dark:text-gray-400 dark:hover:text-gray-700  dark:border-l-gray-400 dark:hover:border-l-gray-500  cursor-pointer">
                     <a href="/#operacje-na-adresach-plikow">Operacja na adresach</a>
+                </li>
+            </ul>
+
+
+            <ul className="flex flex-col text-center m-2">
+               <li className="text-2xl mt-1 ml-4 w-fit p-1 pt-0 pb-0 border-l border-l-gray-400 hover:border-black dark:border-l-white dark:hover:border-l-gray-500  cursor-pointer ">
+                     <a href="/#programy">Programy</a>
+                </li>
+                <li className="mt-1 ml-4 w-fit p-1 pt-0 pb-0 border-l text-gray-800 hover:text-gray-400   border-l-gray-400 hover:border-black dark:text-gray-400 dark:hover:text-gray-700  dark:border-l-gray-400 dark:hover:border-l-gray-500  cursor-pointer">
+                    <a href="/#operacja-nano">Edytor Nano</a>
                 </li>
             </ul>
         </div>
