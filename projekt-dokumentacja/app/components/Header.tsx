516a3cef43fd15868e8d13dfeedee46a8531fa48
filip <filipaleksandrowicz5@gmail.com>
2025-11-18 16:07:33 +0100

    Poprawiłem wszystko i chyba to koniec

--- projekt-dokumentacja/app/components/Header.tsx
+++ projekt-dokumentacja/app/components/Header.tsx
@@ -45,7 +45,7 @@ export default function Header({ onMenuToggle, isMobileMenuOpen }: HeaderProps){
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 sm:w-6 sm:h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
                         </svg>
-                        <span className="hidden sm:inline">Donate</span>
+                        <span className="inline">Donate</span>
                     </Link>
                     <div className="mr-2 sm:mr-6 h-full flex items-center">
                         {/* księżyc - tylko w dark mode */}
